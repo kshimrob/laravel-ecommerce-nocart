d2079e6b1337cc18ada56a6ef982a93d4f52c275
Kelly Robinson <kshimrob@gmail.com>
2018-10-19 10:24:20 -0500

    styled search events component

--- resources/views/components/searchevents.blade.php
+++ resources/views/components/searchevents.blade.php
@@ -1,4 +1,4 @@
-<div class="purchase-tickets">
+<div class="purchase-tickets" style="background-image:url({{ asset('img/search.png') }})">
     <div>
       <h2>Ready to Buy Your Tickets?</h2>
       <p>Lorem ipsum dolor amet vinyl cornhole chambray ramps. Coloring book forage pug microdosing migas, flexitarian semiotics. </p>
